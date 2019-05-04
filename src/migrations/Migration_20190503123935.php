243afc96f47985f0a515cece7d9b94354d840aaa
S.Zares <sergiozares@gmail.com>
2019-05-04 19:34:33 +0300

    LUCWFISCQ2SR8B7BT4BPRN24

--- src/migrations/Migration_20190503123935.php
+++ src/migrations/Migration_20190503123935.php
@@ -21,21 +21,21 @@ class Migration_20190503123935 extends Migration
 		$this->getConnection()->query
 		(
 			"CREATE TABLE `profiles` (
-			  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-			  `user_id` int(11) unsigned NOT NULL,
-			  `first_name` varchar(255) NOT NULL DEFAULT '',
-			  `last_name` varchar(255) NOT NULL DEFAULT '',
-			  `phone` varchar(255) NOT NULL DEFAULT '',
-			  `address` varchar(255) NOT NULL DEFAULT '',
-			  `country_id` int(11) unsigned DEFAULT NULL,
-			  `birthday` date DEFAULT NULL,
-			  `avatar` varchar(255) NOT NULL DEFAULT '',
-			  `image` varchar(255) NOT NULL DEFAULT '',
-			  `last_login` datetime DEFAULT NULL,
-			  `last_ip` varchar(255) NOT NULL DEFAULT '',
-			  PRIMARY KEY (`id`),
-			  KEY `user_id` (`user_id`),
-			  CONSTRAINT `profiles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+				`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+				`user_id` int(11) unsigned NOT NULL,
+				`first_name` varchar(255) NOT NULL DEFAULT '',
+				`last_name` varchar(255) NOT NULL DEFAULT '',
+				`phone` varchar(255) NOT NULL DEFAULT '',
+				`address` varchar(255) NOT NULL DEFAULT '',
+				`country_id` char(2) NOT NULL DEFAULT '',
+				`birthday` date DEFAULT NULL,
+				`avatar` varchar(255) NOT NULL DEFAULT '',
+				`image` varchar(255) NOT NULL DEFAULT '',
+				`last_login` datetime DEFAULT NULL,
+				`last_ip` varchar(255) NOT NULL DEFAULT '',
+				PRIMARY KEY (`id`),
+				KEY `user_id` (`user_id`),
+				CONSTRAINT `profiles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 			) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;"
 		);
 	}
--- src/migrations/Migration_20190503131311.php
+++ src/migrations/Migration_20190503131311.php
@@ -21,24 +21,15 @@ class Migration_20190503131311 extends Migration
 		$this->getConnection()->query
 		(
 			"CREATE TABLE `countries` (
-			  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-			  `capital` varchar(255) DEFAULT NULL,
-			  `citizenship` varchar(255) DEFAULT NULL,
-			  `country_code` varchar(3) NOT NULL DEFAULT '',
-			  `currency` varchar(255) DEFAULT NULL,
-			  `currency_code` varchar(255) DEFAULT NULL,
-			  `currency_sub_unit` varchar(255) DEFAULT NULL,
-			  `currency_symbol` varchar(3) DEFAULT NULL,
-			  `full_name` varchar(255) DEFAULT NULL,
-			  `iso_3166_2` varchar(2) NOT NULL DEFAULT '',
-			  `iso_3166_3` varchar(3) NOT NULL DEFAULT '',
-			  `name` varchar(255) NOT NULL DEFAULT '',
-			  `region_code` varchar(3) NOT NULL DEFAULT '',
-			  `sub_region_code` varchar(3) NOT NULL DEFAULT '',
-			  `eea` tinyint(1) NOT NULL DEFAULT '0',
-			  `calling_code` varchar(3) DEFAULT NULL,
-			  `flag` varchar(6) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-			  PRIMARY KEY (`id`)
+				`id` char(2) NOT NULL,
+				`name` varchar(255) NOT NULL,
+				`official_name` varchar(255) NOT NULL DEFAULT '',
+				`native_name` varchar(255) NOT NULL DEFAULT '',
+				`native_official_name` varchar(255) NOT NULL DEFAULT '',
+				`iso_3166_1_alpha2` char(2) NOT NULL,
+				`iso_3166_1_alpha3` char(3) NOT NULL,
+				`calling_code` int(10) unsigned DEFAULT NULL,
+				PRIMARY KEY (`id`)
 			) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;"
 		);
 	}
